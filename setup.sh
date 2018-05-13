dce8d2c514ad1ccec26d0fa1f670f49fffc7b9e7
harringtonjd0 <30322040+harringtonjd0@users.noreply.github.com>
2018-05-13 17:15:25 -0400

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-## Set up Flask app in /opt and create 'pi-webapp' alias
+## Set up Flask app in /opt and create /usr/bin link to assistant script
 
-# Check if app directory exists. If so, empty it
+# Check if app directory exists. If so, empty it.
 if [ -d "/opt/assistant" ]; then
 	rm -rf "/opt/assistant/"
 fi
@@ -10,13 +10,15 @@ fi
 # Copy app contents into directory
 mkdir /opt/assistant
 cp -R * /opt/assistant
+chmod 0755 /opt/assistant/app/assistant
 
 echo "[+] Placed Flask app in /opt/assistant"
 
-# Check if pi-webapp link exists. If not, create it.
+# Check if assistant link exists. If not, create it.
 if [ ! -L "/usr/bin/assistant" ]; then
 	ln -s /opt/assistant/app/assistant /usr/bin/assistant
 fi
 
 echo "[+] Use command 'assistant' to run server."
+echo "Usage: assistant [ start | stop | status ]"
 
